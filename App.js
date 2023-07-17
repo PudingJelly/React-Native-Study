c420afe100d4cde769fbaa59bca6b07b80a4449f
changju <changju7857@gmail.com>
2023-07-17 15:17:31 +0900

    #4. GoalInput 컴포넌트 분리, 삭제로직 작성 중

--- App.js
+++ App.js
@@ -10,41 +10,34 @@ import {
 } from "react-native";
 
 import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
   // 사용자의 입력값 상태 관리 변수
-  const [enteredGoalText, setEnteredGoalText] = useState("");
-
-  const [goalLists, setGoalLists] = useState([]);
-
-  // 사용자가 내용을 입력할 때, 해당 입력값을 가져오는 함수
-  const goalInputHandler = (enteredText) => {
-    // console.log(enteredText);
-    setEnteredGoalText(enteredText);
-  };
+  const [todoGoals, setTodoGoals] = useState([]);
 
   // 버튼을 누르면 할 일 목록을 추가하는 함수
-  const addGoalHandler = () => {
+  const addGoalHandler = (enteredGoalText) => {
     // console.log(enteredTodoText);
 
     // useState로 관리하는 상태 변수의 setter 안에 콜백 함수를 작성하면,
     // 그 콜백 함수의 매개값은 항상 해당 상태 변수의 최신 값이 전달됩니다.
-    setGoalLists((currentGoalLists) => [
-      ...currentGoalLists,
+    setTodoGoals((currentTodoGoals) => [
+      ...currentTodoGoals,
       { text: enteredGoalText, id: Math.random.toString() },
     ]);
   };
 
+  const deleteGoalHandler = (id) => {
+    // console.log("DELETE!");
+    setTodoGoals((currentTodoGoals) => {
+      return currentTodoGoals.filter((goal) => goal.id !== id);
+    });
+  };
+
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="할 일을 입력하세요!"
-          onChangeText={goalInputHandler}
-        />
-        <Button title="할 일 추가하기" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalContainer}>
         {/* ScrollView는 전체 화면이 렌더링 될 때 안의 항목들을 전부 렌더링 합니다.
             이로 인해, 성능의 저하가 발생할 수 있습니다.
@@ -52,9 +45,14 @@ export default function App() {
             FlatList는 보이는 영역만 일단 렌더링을 진행하고, 나머지 항목들은 스크롤이 움직임이
             발생하면 렌더링을 진행합니다. */}
         <FlatList
-          data={goalLists}
+          data={todoGoals}
           renderItem={(itemData) => {
-            return <GoalItem text={itemData.item.text} />;
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
           }}
           keyExtractor={(item, index) => {
             return item.id;
@@ -74,22 +72,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16, // 좌우 패딩 동시 지정
     flex: 1,
   },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
-    flex: 1,
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
-    padding: 8,
-  },
+
   goalContainer: {
     flex: 4,
   },
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,11 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 const GoalItem = (props) => {
   return (
     // react-native는 스타일 상속의 개념이 없어서 각각 스타일을 지정해야 한다
-    <View style={styles.goalItem}>
-      <Text style={styles.goalText}>{props.text}</Text>
-    </View>
+    // 하나의 목록을 터치할 수 있도록 해주는 기능 Pressable
+    <Pressable onPress={props.onDeleteItem}>
+      <View style={styles.goalItem}>
+        <Text style={styles.goalText}>{props.text}</Text>
+      </View>
+    </Pressable>
   );
 };
 
